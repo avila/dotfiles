2a448744aa6f71dd891054e36b562c3a7a734f71
mr-avila <31955632+mr-avila@users.noreply.github.com>
2018-11-09 18:19:26 +0100

    workaround for rotated pages

--- scripts/insertBlank.sh
+++ scripts/insertBlank.sh
@@ -10,10 +10,23 @@ then
   exit $E_BADARGS
 else
   NUM=$(pdftk $1 dump_data | grep 'NumberOfPages' | awk '{split($0,a,": "); print a[2]}')
+  PGROT=$(pdfinfo $1 | grep 'Page rot' | sed 's/[^0-9]*//g')
   PAGEW=$(pdfinfo $1 | grep 'Page size' | awk '{split($0,a," "); print a[3]}')
   PAGEH=$(pdfinfo $1 | grep 'Page size' | awk '{split($0,a," "); print a[5]}')
+  echo "Number of Pages: $NUM"
+  echo "Page Rotation: $PGROT"
+  echo "Page Width: $PAGEW"
+  echo "Page Height: $PAGEH"
 
   COMMSTR=''
+  
+  if [ $PGROT = 90 ] || [ $PGROT =-90 ]
+  #checks if page is rotated (landscape); then inverts Width and Height
+  then 
+  	PAGEW2=$PAGEW
+  	PAGEW=$PAGEH
+  	PAGEH=$PAGEW2
+  fi
 
   for i in $(seq 1 $NUM);
 	  do
