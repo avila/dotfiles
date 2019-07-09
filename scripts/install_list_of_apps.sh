dc3d6c037d015a876e8c7105b26869ae7f20cb0f
mr-avila <31955632+mr-avila@users.noreply.github.com>
2019-07-09 21:11:04 +0200

    Create install_list_of_apps.sh

---
+++ scripts/install_list_of_apps.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+## declare an array variable
+declare -a lisf_of_apps=("R" "rstudio")
+
+for app in "${lisf_of_apps[@]}"
+do
+	echo "Do you wish to install $app ?"
+	select yn in "Yes" "No" "QUIT"; 
+	do
+    	case $yn in
+   	    	Yes ) apt install $app; break;; # maybe needs sudo apt, or run 
+	        No ) break;;                    # script as sudo
+    	    QUIT ) exit;;
+    	esac
+	done
+done
+
+#adapted from: https://stackoverflow.com/questions/226703/
+#and https://stackoverflow.com/questions/8880603
